39569e769280648ef6b25d01c7e5a070949d967d
Pirulo23 <147708352+Pirulo23@users.noreply.github.com>
2026-03-01 16:07:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from analizador import analizar_ticket_ocr
+import os
 
-app = Flask(name)
+app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
 def inicio():
-return "Motor de Premium Hospital en linea."
+    return "Motor de Premium Hospital funcionando."
 
 @app.route('/procesar_ticket', methods=['POST'])
 def procesar_ticket():
-datos = request.json
-texto_crudo = datos.get('texto_ocr', '')
-resultados_limpios = analizar_ticket_ocr(texto_crudo)
-return jsonify(resultados_limpios)
+    datos = request.json
+    texto_crudo = datos.get('texto_ocr', '')
+    resultados_limpios = analizar_ticket_ocr(texto_crudo)
+    return jsonify(resultados_limpios)
 
-if name == 'main':
-app.run(debug=True, port=5000)
+if __name__ == '__main__':
+    puerto = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=puerto)
